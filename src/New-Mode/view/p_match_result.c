070316892db9ff8b5fd892dbd587979299aba6a3
CptPackage <abdelrhmanabdalla15@gmail.com>
2022-03-19 23:05:24 +0100

    Finished the Match Result

--- src/New-Mode/view/p_match_result.c
+++ src/New-Mode/view/p_match_result.c
@@ -5,20 +5,32 @@
 #include <stdlib.h>
 
 void view_match_result(match_result_t match_result) {
-  print_char_line('-');
-  switch (match_result) {
-  // QUIT
-  case 0:
-    print_framed_text("YOU QUIT THE MATCH!", '|', false);
-    break;
-  // LOST
-  case 1:
-    print_framed_text("CONGRATULATIONS! YOU WON!", '|', false);
-    break;
-  // WON
-  case 2:
-    print_framed_text("YOU LOST!", '|', false);
-    break;
+  bool result;
+  while (!result) {
+    clear_screen();
+    print_char_line('-');
+    print_framed_text("", '|', false);
+    switch (match_result) {
+    // QUIT
+    case 0:
+      print_framed_text("YOU QUIT THE MATCH!", '|', false);
+      break;
+
+    // LOST
+    case 1:
+      print_framed_text("CONGRATULATIONS! YOU WON!", '|', false);
+      break;
+
+    // WON
+    case 2:
+      print_framed_text("YOU LOST!", '|', false);
+      break;
+    case 3:
+      print_framed_text("UNKNOWN match_result_t!", '|', false);
+      break;
+    }
+    print_framed_text("", '|', false);
+    print_char_line('-');
+    result = yes_or_no("Go to Main Menu", 'y', 'n', true, true);
   }
-  print_char_line('-');
 }
