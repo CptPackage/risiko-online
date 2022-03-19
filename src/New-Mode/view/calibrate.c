346026d855c0db836846cecc45aefbe49e32ef73
CptPackage <abdelrhmanabdalla15@gmail.com>
2022-03-19 22:57:28 +0100

    Fixed args bug in calibrate view

--- src/New-Mode/view/calibrate.c
+++ src/New-Mode/view/calibrate.c
@@ -8,9 +8,10 @@ void view_calibrate() {
     clear_screen();
     print_star_line();
     print_framed_text(
-        "Type [y] if you can see the whole UI in one line un-deformed", '-');
+        "Type [y] if you can see the whole UI in one line un-deformed", '|',
+        false);
     print_framed_text("Otherwise resize your screen and type [r] to re-render",
-                      '-');
+                      '|', false);
     print_star_line();
     result = yes_or_no("Do you want to proceed or re-render?", 'y', 'r', false,
                        true);
