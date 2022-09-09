1de4dd158fb20f33778e4253ca24609600c8c60e
CptPackage <abdelrhmanabdalla15@gmail.com>
2022-09-09 18:59:47 +0200

    Added GetHistory for Players

--- doc/ID BASED/SQL Scripts/[Testing] Create Room and Auto Join.sql
+++ doc/ID BASED/SQL Scripts/[Testing] Create Room and Auto Join.sql
@@ -7,13 +7,25 @@ CALL JoinMatch(@RoomNumber,'player5',@JoinedRoom);
 SELECT * FROM `Match` WHERE matchNumber = @MatchNum;
 SELECT * FROM Turn;
 SELECT * FROM `Match`;
+SELECT * FROM Ingame_Players;
 SELECT * FROM To_Check_Matches;
 DROP TEMPORARY TABLE Full_Scan_Logs;
 DROP TEMPORARY TABLE To_Check_Matches;
 DROP TEMPORARY TABLE Current_Check_Turns;
-
+SET @MatchNum = 3;
 SHOW EVENTS;
 CALL AbandonMatch(@MatchNum,'player1');
 CALL AbandonMatch(@MatchNum,'player4');
 CALL AbandonMatch(@MatchNum,'player2');
 CALL AbandonMatch(@MatchNum,'player5');
+
+
+SELECT IP.matchNumber AS 'Match', IP.player AS 'Player',
+(M.matchStartCountdown + INTERVAL 2 MINUTE) AS 'Match Start Time',
+CASE WHEN IP.winner = 1 THEN 'Won' ELSE 'Eliminated' END as 'Result',
+CASE WHEN IP.winner THEN M.matchEndTime ELSE IP.exitTime END as 'Exit Time'
+FROM Ingame_Players AS IP
+JOIN `Match` AS M
+ON IP.matchNumber = M.matchNumber
+WHERE player = 'player1'
+AND (exitTime IS NOT NULL OR winner = 1);
